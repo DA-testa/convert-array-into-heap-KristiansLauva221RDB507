07167b5c65c2ed6750cfba7607757f32f1db515f
KristiansLauva221RDB507 <125013016+KristiansLauva221RDB507@users.noreply.github.com>
2023-03-06 23:31:57 +0200

    Add files via upload

--- build_heap.py
+++ build_heap.py
@@ -1,42 +1,37 @@
-# python3
+def sift_down(data, i, swaps):
+    """
+    Performs sift-down operation on the element at index i in the array.
+    Modifies the array in place and records the swaps made in the swaps list.
+    """
+    min_index = i
+    left_child = 2 * i + 1
+    if left_child < len(data) and data[left_child] < data[min_index]:
+        min_index = left_child
+    right_child = 2 * i + 2
+    if right_child < len(data) and data[right_child] < data[min_index]:
+        min_index = right_child
+    if i != min_index:
+        data[i], data[min_index] = data[min_index], data[i]
+        swaps.append((i, min_index))
+        sift_down(data, min_index, swaps)
 
 
 def build_heap(data):
+    """
+    Builds a min-heap from the given array using the sift-down method.
+    Returns a list of swaps made during the heap construction.
+    """
     swaps = []
-    # TODO: Creat heap and heap sort
-    # try to achieve  O(n) and not O(n2)
-
-
+    for i in range(len(data) // 2, -1, -1):
+        sift_down(data, i, swaps)
     return swaps
 
 
-def main():
-    
-    # TODO : add input and corresponding checks
-    # add another input for I or F 
-    # first two tests are from keyboard, third test is from a file
-
-
-    # input from keyboard
+if __name__ == "__main__":
     n = int(input())
     data = list(map(int, input().split()))
-
-    # checks if lenght of data is the same as the said lenght
     assert len(data) == n
-
-    # calls function to assess the data 
-    # and give back all swaps
     swaps = build_heap(data)
-
-    # TODO: output how many swaps were made, 
-    # this number should be less than 4n (less than 4*len(data))
-
-
-    # output all swaps
     print(len(swaps))
     for i, j in swaps:
         print(i, j)
-
-
-if __name__ == "__main__":
-    main()
