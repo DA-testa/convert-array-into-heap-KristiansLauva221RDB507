d3ff6503a0499a58237acf43b9f85af84e214490
KristiansLauva221RDB507 <125013016+KristiansLauva221RDB507@users.noreply.github.com>
2023-03-07 00:16:21 +0200

    Add files via upload

--- build_heap.py
+++ build_heap.py
@@ -1,41 +1,3 @@
-# import sys
-
-# def sift_down(data, i, swaps):
-#     """
-#     Performs sift-down operation on the element at index i in the array.
-#     Modifies the array in place and records the swaps made in the swaps list.
-#     """
-#     min_index = i
-#     left_child = 2 * i + 1
-#     if left_child < len(data) and data[left_child] < data[min_index]:
-#         min_index = left_child
-#     right_child = 2 * i + 2
-#     if right_child < len(data) and data[right_child] < data[min_index]:
-#         min_index = right_child
-#     if i != min_index:
-#         data[i], data[min_index] = data[min_index], data[i]
-#         swaps.append((i, min_index))
-#         sift_down(data, min_index, swaps)
-
-
-# def build_heap(data):
-#     """
-#     Builds a min-heap from the given array using the sift-down method.
-#     Returns a list of swaps made during the heap construction.
-#     """
-#     swaps = []
-#     for i in range(len(data) // 2, -1, -1):
-#         sift_down(data, i, swaps)
-#     return swaps
-
-
-# if __name__ == "__main__":
-#     n = int(sys.stdin.readline())
-#     data = map(int, sys.stdin.readline().split())
-#     swaps = build_heap(list(data))
-#     print(len(swaps))
-#     for i, j in swaps:
-#         print(i, j)
 import sys
 
 def sift_down(data, i, swaps):
@@ -67,7 +29,7 @@ def build_heap(data, n):
     return swaps
 
 
-if __name__ == "__main__":
+def main():
     input_type = input()
     if "F" in input_type:
         filename = input()
@@ -81,7 +43,6 @@ if __name__ == "__main__":
     elif "I" in input_type:
         n = int(input())
         data = list(map(int, input().split()))
-
     # checks if length of data is the same as the said length
     assert len(data) == n
 
@@ -96,3 +57,6 @@ if __name__ == "__main__":
     print(len(swaps))
     for i, j in swaps:
         print(i, j)
+
+if __name__ == "__main__":
+    main()
