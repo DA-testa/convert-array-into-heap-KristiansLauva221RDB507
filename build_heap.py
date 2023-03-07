7034c726beb00c56f58d9e786d36df07d4881d1b
KristiansLauva221RDB507 <125013016+KristiansLauva221RDB507@users.noreply.github.com>
2023-03-07 22:38:23 +0200

    Add files via upload

--- build_heap.py
+++ build_heap.py
@@ -1,16 +1,24 @@
-import sys
-
-
 def build_heap(data):
-   
     swaps = []
-    for i in range(len(data) -1,- 1,-1):
-        while i!= 0:
-            if data[i]<data[int((i-1)/2)]:
-                data[i],data[int((i-1)/2)] = data[int((i-1)/2)],data[i]
-                swaps.append((int((i-1)/2),i))
-            i = int((i-1)/2)
+    for i in range(len(data) // 2, -1, -1):
+        sift_down(i, data, swaps)
     return swaps
+
+def sift_down(i, data, swaps):
+    min_index = i
+    left_child = 2 * i + 1
+    right_child = 2 * i + 2
+
+    if left_child < len(data) and data[left_child] < data[min_index]:
+        min_index = left_child
+    if right_child < len(data) and data[right_child] < data[min_index]:
+        min_index = right_child
+
+    if i != min_index:
+        data[i], data[min_index] = data[min_index], data[i]
+        swaps.append((i, min_index))
+        sift_down(min_index, data, swaps)
+
 def main():
     input_type = input()
     if "I" in input_type:
@@ -19,22 +27,17 @@ def main():
     elif "F" in input_type:
         filename = input()
         if "a" not in filename:
-           with open("./tests/"+filename, mode='r') as f:
+            with open("./tests/"+filename, mode='r') as f:
                 nav = int(f.readline())
                 data = list(map(int, f.readline().split()))
     else:
         print("error")
+        return
     
-    # checks if length of data is the same as the said length
     assert len(data) == nav
-
     
     swaps = build_heap(data)
 
-    # TODO: output how many swaps were made, 
-    
-
-    # output all swaps
     print(len(swaps))
     for i, j in swaps:
         print(i, j)
