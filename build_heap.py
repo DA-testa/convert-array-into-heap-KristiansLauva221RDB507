8dd790ca285897a67c480875a9583fc4267778ca
KristiansLauva221RDB507 <125013016+KristiansLauva221RDB507@users.noreply.github.com>
2023-03-07 22:34:01 +0200

    Add files via upload

--- build_heap.py
+++ build_heap.py
@@ -4,26 +4,27 @@ import sys
 def build_heap(data):
    
     swaps = []
-    for i in range(len(data) -1, -1, -1):
+    for i in range(len(data) -1,- 1,-1):
         while i!= 0:
             if data[i]<data[int((i-1)/2)]:
                 data[i],data[int((i-1)/2)] = data[int((i-1)/2)],data[i]
-                swaps.append((i,int((i-1)/2)))
+                swaps.append((int((i-1)/2),i))
             i = int((i-1)/2)
     return swaps
 def main():
     input_type = input()
-    if "F" in input_type:
-        filename = input()
-        if "a" not in filename:
-           with open("./tests/"+filename, mode="r") as f:
-                nav = int(f.readline())
-                data = list(map(int, f.readline().split()))
-        else:
-            print("error")
-    elif "I" in input_type:
+    if "I" in input_type:
         nav = int(input())
         data = list(map(int, input().split()))
+    elif "F" in input_type:
+        filename = input()
+        if "a" not in filename:
+           with open("./tests/"+filename, mode='r') as f:
+                nav = int(f.readline())
+                data = list(map(int, f.readline().split()))
+    else:
+        print("error")
+    
     # checks if length of data is the same as the said length
     assert len(data) == nav
 
